bc1ab1146f2c08d10f4ff49439f7d87f9fbf7f02
jolly <none@none>
2009-05-04 09:57:24 -0400

    inital import of flow utilitities and tools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='flowpy',
+      version='0.01',
+      py_modules=['flowdata'],
+      package_dir = {'': 'src'},
+      description='Python Flow Cytometry Tools',
+      author='Jacob Frelinger',
+      author_email='jacob.frelinger@duke.edu',
+      )
