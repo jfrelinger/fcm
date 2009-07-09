6ea9abab13cdc04376f9b22a506c334d46daf0c5
cliburn@paster.local <cliburn@paster.local>
2009-07-09 09:39:12 -0400

    add points_in_poly function for gating

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,4 +1,5 @@
 from fcmdata import FCMdata
 from fcmannotation import Annotation
 from fcmexceptions import BadFCMPointDataTypeError, UnimplementedFcsDataMode
-from fcmtransforms import productlog
+from fcmtransforms import productlog, logicle, hyperlog
+from fcmgate import points_in_poly
