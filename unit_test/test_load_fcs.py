78271e7cfdddcc0b79398d33ac8c43660c02a90b
cliburn@lister.dulci.duhs.duke.edu <cliburn@lister.dulci.duhs.duke.edu>
2009-07-25 10:50:20 -0400

    fix test_load_fcs.py to work with new API

--- unit_test/test_load_fcs.py
+++ unit_test/test_load_fcs.py
@@ -6,10 +6,10 @@ class FCSreaderTestCase(unittest.TestCase):
         self.fcm = FCSreader('../sample_data/3FITC_4PE_004.fcs').get_FCMdata()
         
     def testGetPnts(self):
-        self.assertEqual(self.fcm.pnts.shape, (94569, 4))
+        self.assertEqual(self.fcm.shape, (94569, 4))
 
-    def testGetAnnotation(self):
-        self.assertEqual(self.fcm.annotation.text['cyt'], 'FACScan')
+    def testGetNotes(self):
+        self.assertEqual(self.fcm.notes.text['cyt'], 'FACScan')
 
 if __name__ == '__main__':
     suite1 = unittest.makeSuite(FCSreaderTestCase,'test')
