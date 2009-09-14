98087eea78442ac216d4912211a82640ea442d44
"Jacob Frelinger ext:(%22) <jacob.frelinger@duke.edu>
2009-09-14 08:34:34 -0400

    prep for google push

--- unit_test/unit_tests.py
+++ unit_test/unit_tests.py
@@ -5,7 +5,7 @@ Unit test framework
 import unittest
 from test_Annotation import FCMAnnotationTestCase
 from test_FCM_data import FCMdataTestCase
-from test_load_lmd import FCSreaderLMDTestCase
+#from test_load_lmd import FCSreaderLMDTestCase
 from test_transforms import FCMtransformTestCase
 from test_load_fcs import FCSreaderTestCase
 from test_tree  import TreeTestCase
@@ -15,12 +15,12 @@ from test_subsample import SubsampleTestCase
 if __name__ == "__main__":
     suite1 = unittest.makeSuite(FCMdataTestCase,'test')
     suite2 = unittest.makeSuite(FCMAnnotationTestCase,'test')
-    suite3 = unittest.makeSuite(FCSreaderLMDTestCase, 'test')
+    #suite3 = unittest.makeSuite(FCSreaderLMDTestCase, 'test')
     suite4 = unittest.makeSuite(FCMtransformTestCase, 'test')
     suite5 = unittest.makeSuite(FCSreaderTestCase,'test')
     suite6 = unittest.makeSuite(TreeTestCase, 'test')
     suite7 = unittest.makeSuite(SubsampleTestCase, 'test')
-    alltests = unittest.TestSuite((suite1, suite2, suite3, suite4, suite5, suite6, suite7))
+    alltests = unittest.TestSuite((suite1, suite2, suite4, suite5, suite6, suite7))
 
     unittest.main()
 
