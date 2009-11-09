631af0f8d02986e4b905874285c9f95ad1f60df3
"Jacob Frelinger ext:(%22) <jacob.frelinger@duke.edu>
2009-11-09 12:42:02 -0500

    fill out details of test_cluster a little more.

--- unit_test/test_cluster.py
+++ unit_test/test_cluster.py
@@ -1,16 +1,28 @@
 from fcm import loadFCS
 from fcm.statistics import DPMixtureModel
-from pylab import scatter, show
-import numpy
-import pickle
+from pylab import scatter, show, subplot
+
 
 if __name__ == '__main__':
+    #load data
     data = loadFCS('/home/jolly/Projects/fcm/sample_data/3FITC_4PE_004.fcs')
+    
+    # initalize model
     model = DPMixtureModel(data, 20, itter=100, burnin=10)
+    
+    # fit model
     model.fit(verbose=True)
+    
+    # get results
     baz = model.get_results()
     
-    scatter(data[:,0], data[:,1], s=.1)
-    scatter(baz.mus()[:,0], baz.mus()[:,1], c='r')
+    # pull out all means
+    mus = baz.mus()
+    
+    #plot results.
+    for i,j,k in [(1,0,1),(2,0,2),(3,0,3), (4,2,3)]:
+        subplot(2,2,i)
+        scatter(data[:,j], data[:,k], s=.1)
+        scatter(mus[:,j], mus[:,k], c='r')
         
     show()
