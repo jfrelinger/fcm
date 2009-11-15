46c3c3759c1a44f52673965bddf66f61d41726a6
Jacob Frelinger <jacob.frelinger@duke.edu>
2009-11-15 12:34:13 -0500

    while I'm in the FCMdata unit test, fix misuse of numpy.all() and make a unit test actually test what it should.

--- unit_test/test_FCM_data.py
+++ unit_test/test_FCM_data.py
@@ -90,7 +90,7 @@ class FCMdataTestCase(unittest.TestCase):
         cols = [0,1]
         g = PolyGate(verts, cols)
         self.fcm.gate(g).gate(g)
-        assert self.fcm.view().all() == array([[0,1,2]]).all(), 'gate excluded wrong points'
+        self.assertTrue( all(self.fcm.view()== array([[0,1,2]])), 'gate excluded wrong points')
         
     def testGetAttr(self):
         assert self.fcm.shape == (2,3), '__gettattr__ failed to deligate'
