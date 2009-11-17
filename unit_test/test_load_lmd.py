9d442bf7a80fb6f5c023e220516302f7688592a0
Jacob Frelinger <jacob.frelinger@duke.edu>
2009-11-17 12:32:17 -0500

    add lmd data and reconnect lmd unit test case to unit tests

--- unit_test/test_load_lmd.py
+++ unit_test/test_load_lmd.py
@@ -3,10 +3,10 @@ from io import FCSreader
 
 class FCSreaderLMDTestCase(unittest.TestCase):
     def setUp(self):
-        self.fcm = FCSreader('/home/jolly/Projects/fcm/sample_data/ICS2D1S1R1lab4.LMD').get_FCMdata()
+        self.fcm = FCSreader('../sample_data/coulter.lmd').get_FCMdata()
         
     def testGetPnts(self):
-        self.assertEqual(self.fcm.shape, (261946, 7))
+        self.assertEqual(self.fcm.shape, (18110, 8))
 
     def testGetNotes(self):
         self.assertEqual(self.fcm.notes.text['cyt'], 'Cytomics FC 500')
--- unit_test/unit_tests.py
+++ unit_test/unit_tests.py
@@ -5,7 +5,7 @@ Unit test framework
 import unittest
 from test_Annotation import FCMAnnotationTestCase
 from test_FCM_data import FCMdataTestCase
-#from test_load_lmd import FCSreaderLMDTestCase
+from test_load_lmd import FCSreaderLMDTestCase
 from test_transforms import FCMtransformTestCase
 from test_load_fcs import FCSreaderTestCase
 from test_tree  import TreeTestCase
@@ -16,13 +16,13 @@ from test_dpmixture import Dp_clusterTestCase
 if __name__ == "__main__":
     suite1 = unittest.makeSuite(FCMdataTestCase,'test')
     suite2 = unittest.makeSuite(FCMAnnotationTestCase,'test')
-    #suite3 = unittest.makeSuite(FCSreaderLMDTestCase, 'test')
+    suite3 = unittest.makeSuite(FCSreaderLMDTestCase, 'test')
     suite4 = unittest.makeSuite(FCMtransformTestCase, 'test')
     suite5 = unittest.makeSuite(FCSreaderTestCase,'test')
     suite6 = unittest.makeSuite(TreeTestCase, 'test')
     suite7 = unittest.makeSuite(SubsampleTestCase, 'test')
     suite8 = unittest.makeSuite(Dp_clusterTestCase, 'test')
-    alltests = unittest.TestSuite((suite1, suite2, suite4, suite5, suite6, suite7, suite8))
+    alltests = unittest.TestSuite((suite1, suite2, suite3, suite4, suite5, suite6, suite7, suite8))
 
     unittest.main()
 
