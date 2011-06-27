c9b86958712ad669d0fac3258a140cbb2ab91def
"Jacob Frelinger ext:(%22) <jacob.frelinger@duke.edu>
2011-06-27 11:37:55 -0400

    add threshold convience plots.

--- src/graphics/plot_gate.py
+++ src/graphics/plot_gate.py
@@ -10,18 +10,107 @@ def plot_gate(data, gate, ax, chan=None, name=None, **kwargs):
     if isinstance(gate,PolyGate):
         plot_poly_gate(data, gate, ax, chan, name, **kwargs)
     elif isinstance(gate,ThresholdGate):
-        if isinstance(chan,int) or len(chan) == 1:
+        if isinstance(chan,int) or chan is None:
             plot_threshold_hist(data, gate, ax, chan, name, **kwargs)
         else:
             plot_threshold_gate(data, gate, ax, chan, name, **kwargs)
             
     
 def plot_threshold_gate(data, gate, ax, chan=None, name=None, **kwargs):
-    pass
+   
+    if 'bgc' in kwargs:
+        bgc = kwargs['bgc']
+        del kwargs['bgc']
+    else:
+        bgc = 'grey'
+        
+    if 'bgalpha' in kwargs:
+        bga = kwargs['bgalpha']
+        del kwargs['bgalpha']
+    else:
+        bga = 1
+
+    if 'c' in kwargs:
+        z = kwargs['c']
+        del kwargs['c'] # needed incase bgc is used
+        calc_z = False
+    else:
+        calc_z = True
+        
+    if 'alpha' in kwargs:
+        alpha = kwargs['alpha']
+        del kwargs['alpha']
+    else:
+        alpha = 1 # is there a way to look up what this defaults to incase this is changed in .matplotlibrc?
+
+    # add in support for specifing size
+    
+    ax.scatter(data[:,chan[0]],data[:,chan[1]], c=bgc, s=1, edgecolor='none', alpha=bga)
+    gate.gate(data, name=name)
+    
+    #has to be set after gating...
+
+    if calc_z:
+        z = bilinear_interpolate(data[:, chan[0]], data[:, chan[1]])  
+
+    ax.scatter(data[:,chan[0]],data[:,chan[1]], c=z, s=1, edgecolor='none', alpha=alpha, **kwargs)
+    
+    if chan[0] == gate.chan:
+        ax.axvline(gate.vert)
+    elif chan[1] == gate.chan:
+        ax.axhline(gate.vert)
 
 
 def plot_threshold_hist(data, gate, ax, chan=None, name=None, **kwargs):
-    pass
+    if 'bgcolor' in kwargs:
+        bgc = kwargs['bgcolor']
+        del kwargs['bgcolor']
+    else:
+        bgc = 'grey'
+        
+    if 'bgalpha' in kwargs:
+        bga = kwargs['bgalpha']
+        del kwargs['bgalpha']
+    else:
+        bga = 1
+
+    if 'color' in kwargs:
+        z = kwargs['color']
+        del kwargs['color'] # needed incase bgc is used
+    else:
+        z = 'blue'
+        
+    if 'alpha' in kwargs:
+        alpha = kwargs['alpha']
+        del kwargs['alpha']
+    else:
+        alpha = 1 # is there a way to look up what this defaults to incase this is changed in .matplotlibrc?
+
+    if 'bins' not in kwargs:
+        kwargs['bins'] = 1000
+        
+    if 'normed' not in kwargs:
+        kwargs['normed'] = True
+        
+    if 'edgecolor' not in kwargs:
+        kwargs['edgecolor'] = 'none'
+
+
+    
+    if chan is None:
+        chan = gate.chan
+    
+    
+    
+    count, bins, patch = ax.hist(data[:,chan], color=bgc, alpha=bga, **kwargs)
+    
+    gate.gate(data, name=name)
+    
+    if 'bins' in kwargs:
+        del kwargs['bins']
+
+    count, bins, patch = ax.hist(data[:,chan], bins=bins, color=z, alpha=alpha, **kwargs)
+    
 
 
 def plot_poly_gate(data, gate, ax, chan=None, name=None, **kwargs):
@@ -74,12 +163,16 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     x = fcm.loadFCS('../../sample_data/3FITC_4PE_004.fcs')
     g = PolyGate(numpy.array([[0,0],[500,0],[500,500],[0,500]]), [0,1])
-    g2 = PolyGate(numpy.array([[0,0],[500,0],[500,500],[0,500]]), [2,3])
+    g2 = ThresholdGate(500,2)
     fig = plt.figure()
-    ax = fig.add_subplot(1,2,1)
+    ax = fig.add_subplot(2,2,1)
     plot_gate(x,g,ax, name="firstgate", alpha=.5, bgalpha=.5)
-    ax = fig.add_subplot(1,2,2)
-    plot_gate(x,g2,ax, name="secondgate", alpha=.5, bgc='red', c='green')
+    ax = fig.add_subplot(2,2,2)
+    plot_gate(x,g2,ax, name="secondgate", chan=(2,3), alpha=.5, bgc='red', c='green')
+    
+    x.visit('root')
+    ax = fig.add_subplot(2,2,3)
+    plot_gate(x,g2,ax, name='thirdgate')
     print x.tree.pprint()
     plt.show()
     
