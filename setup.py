6d2698803b30d9f5737af447e00ca4c40035e606
"Jacob Frelinger ext:(%22) <jacob.frelinger@duke.edu>
2012-05-03 09:28:05 -0400

    remove remnants of cdp/mvnpdf extension

--- setup.py
+++ setup.py
@@ -1,37 +1,6 @@
 from distutils.core import setup, Extension
 from numpy import get_include
 
-mvnpdf_extension = Extension('fcm.statistics._mvnpdf',
-                             sources = ['src/statistics/%s' % i for i in 
-                                ['mvnpdf_ext/mvnpdf.cpp',
-                                'mvnpdf_ext/mvnpdf_wrap.cpp',
-                                'mvnpdf_ext/random/specialfunctions2.cpp',
-                                'mvnpdf_ext/random/SpecialFunctions.cpp',
-                                'mvnpdf_ext/matrix/newmat1.cpp',
-                                'mvnpdf_ext/matrix/newmat2.cpp',
-                                'mvnpdf_ext/matrix/newmat3.cpp',
-                                'mvnpdf_ext/matrix/newmat4.cpp',
-                                'mvnpdf_ext/matrix/newmat5.cpp',
-                                'mvnpdf_ext/matrix/newmat6.cpp',
-                                'mvnpdf_ext/matrix/newmat7.cpp',
-                                'mvnpdf_ext/matrix/newmat8.cpp',
-                                'mvnpdf_ext/matrix/newmat9.cpp',
-                                'mvnpdf_ext/matrix/newmatex.cpp',
-                                'mvnpdf_ext/matrix/myexcept.cpp',
-                                'mvnpdf_ext/matrix/bandmat.cpp',
-                                'mvnpdf_ext/matrix/submat.cpp',
-                                'mvnpdf_ext/matrix/newmatrm.cpp',
-                                'mvnpdf_ext/matrix/svd.cpp',
-                                'mvnpdf_ext/matrix/sort.cpp',
-                                'mvnpdf_ext/matrix/cholesky.cpp']],
-                            include_dirs = [get_include(), 
-                                    'src/statistics/mvnpdf_ext/matrix',
-                                    'src/statistics/mvnpdf_ext/random',
-                                    'src/statistics/mvnpdf_ext'],
-                            #libraries = ['m', 'stdc++']
-                            )
-                                        
-
 logicle_extension = Extension('fcm.core._logicle',
                               sources = [ 'src/core/logicle_ext/%s' %i for i in [
                                 'Logicle.cpp',
@@ -48,7 +17,7 @@ setup(name='fcm',
       description='Python Flow Cytometry (FCM) Tools',
       author='Jacob Frelinger',
       author_email='jacob.frelinger@duke.edu',
-      ext_modules = [logicle_extension, mvnpdf_extension],
+      ext_modules = [logicle_extension],
       requires=['numpy (>=1.3.0)',
                 'scipy (>=0.6)',
 		'dpmix (>=0.1)',
--- unit_test/run_cpu_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln -sf /Users/cliburn/hg/fcm/src/statistics/_cdp_cpu.so /Users/cliburn/hg/fcm/src/statistics/_cdp.so
-python test_cluster.py
