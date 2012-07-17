69cd23f231f25dbc00d2cd078cbe91e9220132bc
Cliburn Chan <cliburn.chan@duke.edu>
2012-07-17 14:20:19 -0400

    remove .pyc and fix import heatmap bug

--- src/graphics/__init__.py
+++ src/graphics/__init__.py
@@ -1,4 +1,4 @@
-from plot import pair_plot, heatmap, hist, heatmaps, contour
+from plot import pair_plot, pseudocolor, pseudocolors, hist, contour
 from util import bilinear_interpolate, trilinear_interpolate, set_logicle
 from plot_gate import plot_gate
 
